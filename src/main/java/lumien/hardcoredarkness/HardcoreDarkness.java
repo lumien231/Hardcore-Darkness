c333547261710ac96d972566404aaf1903c43ac6
Lumien <lumien231@users.noreply.github.com>
2016-12-03 19:05:07 +0100

    lower case mod id

--- src/main/java/lumien/hardcoredarkness/HardcoreDarkness.java
+++ src/main/java/lumien/hardcoredarkness/HardcoreDarkness.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @Mod(modid = HardcoreDarkness.MOD_ID, name = HardcoreDarkness.MOD_NAME, version = HardcoreDarkness.MOD_VERSION)
 public class HardcoreDarkness
 {
-	public static final String MOD_ID = "HardcoreDarkness";
+	public static final String MOD_ID = "hardcoredarkness";
 	public static final String MOD_NAME = "Hardcore Darkness";
 	public static final String MOD_VERSION = "@VERSION@";
 
